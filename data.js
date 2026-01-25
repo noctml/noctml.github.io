22bba9067cae4449f19a7829cb921a0cafb2f10d
noctml <pdwytime@gmail.com>
2026-01-25 21:56:29 +0900

    file path changed

--- data.js
+++ data.js
@@ -3,25 +3,25 @@ window.SITE_DATA = {
             {
                 title: "ORB-SLAM2: an Open-Source SLAM System for Monocular, Stereo and RGB-D Cameras",
                 desc: "Visual SLAM의 정석",
-                href: "pages/paper_reviews/ORB-SLAM2/논문-스터디.html",
+                href: "pages/paper_reviews/ORB-SLAM2/",
                 published: true
             },
             {
                 title: "DROID-SLAM: Deep Visual SLAM for Monocular, Stereo, and RGB-D Cameras",
                 desc: "딥러닝을 이용한 SLAM 최적화 방식 제안",
-                href: "pages/paper_reviews/DROID-SLAM/논문-스터디.html",
+                href: "pages/paper_reviews/DROID-SLAM/",
                 published: true
             },
             {
                 title: "3D Scene Graph: A Structure for Unified Semantics, 3D Space, and Camera",
                 desc: "Sementic SLAM의 기반",
-                href: "pages/paper_reviews/3D_SG/논문-스터디.html",
+                href: "pages/paper_reviews/3D_SG/",
                 published: true
             },
             {
                 title: "3D Dynamic Scene Graphs: Actionable Spatial Perception with Places, Objects, and Humans",
                 desc: "3D Scene Graph에서 동적 객체 처리를 보완",
-                href: "pages/paper_reviews/3D_DSG/논문-스터디.html",
+                href: "pages/paper_reviews/3D_DSG/",
                 published: true
             },
         ],
