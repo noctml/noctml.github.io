a2d9bf91165d7fc7792244ea8c506ec2c589648b
noctml <pdwytime@gmail.com>
2026-01-25 21:17:31 +0900

    data.js added, some paper reviews and projects are also added, and little design changed

---
+++ data.js
@@ -0,0 +1,38 @@
+window.SITE_DATA = {
+        paperReviews: [
+            {
+                title: "ORB-SLAM2: an Open-Source SLAM System for Monocular, Stereo and RGB-D Cameras",
+                desc: "Visual SLAM의 정석",
+                href: "pages/paper_reviews/ORB-SLAM2/논문-스터디.html",
+                published: true
+            },
+            {
+                title: "DROID-SLAM: Deep Visual SLAM for Monocular, Stereo, and RGB-D Cameras",
+                desc: "딥러닝을 이용한 SLAM 최적화 방식 제안",
+                href: "pages/paper_reviews/DROID-SLAM/논문-스터디.html",
+                published: true
+            },
+            {
+                title: "3D Scene Graph: A Structure for Unified Semantics, 3D Space, and Camera",
+                desc: "Sementic SLAM의 기반",
+                href: "pages/paper_reviews/3D_SG/논문-스터디.html",
+                published: true
+            },
+            {
+                title: "3D Dynamic Scene Graphs: Actionable Spatial Perception with Places, Objects, and Humans",
+                desc: "3D Scene Graph에서 동적 객체 처리를 보완",
+                href: "pages/paper_reviews/3D_DSG/논문-스터디.html",
+                published: true
+            },
+        ],
+        projects: [
+        {
+            title: "ORB-SLAM2",
+            desc: "오픈소스 실습",
+            href: "pages/projects/ORB-SLAM2_tut/실습-진행.html",
+            published: true,
+            tags: ["ORB-SLAM2", "Visual SLAM", "GitHub"],
+            thumb: "pages/projects/ORB-SLAM2_tut/assets/실습-진행/실습 진행/view.jpg"
+        },
+    ]
+};
